62804b4c42d166261af681e90feddc752c8fc00b
natalia <natalialqp@gmail.com>
2023-12-08 16:50:48 +0100

    Generalisation of dof

--- src/robot.py
+++ src/robot.py
@@ -41,6 +41,7 @@ class Robot(object):
         self.physical_limits_left = data["real-angles-left-arm"]
         self.physical_limits_right = data["real-angles-right-arm"]
         self.physical_limits_head = data["real-angles-head"]
+        self.dof_arm = data["DOF-right-arm"]
 
     def forward_kinematics_qt(self, angles):
         '''
@@ -156,9 +157,9 @@ class Robot(object):
         rightArmAngles["torso"] = self.rightArmAngles["torso"]
         rightArmAngles["shoulder-pitch"] = self.rightArmAngles["shoulder-pitch"] + np.array([0, angles[5], 0])
         rightArmAngles["shoulder-roll"] = self.rightArmAngles["shoulder-roll"] + np.array([0, 0, angles[6]])
-        rightArmAngles["elbow-yaw"] = self.rightArmAngles["elbow-yaw"] + np.array([-angles[7], 0, 0])
+        rightArmAngles["elbow-yaw"] = self.rightArmAngles["elbow-yaw"] + np.array([angles[7], 0, 0]) #-
         rightArmAngles["elbow-roll"] = self.rightArmAngles["elbow-roll"] + np.array([0, 0, angles[8]])
-        rightArmAngles["wrist-yaw"] = self.rightArmAngles["wrist-yaw"] + np.array([-angles[9], 0, 0])
+        rightArmAngles["wrist-yaw"] = self.rightArmAngles["wrist-yaw"] + np.array([angles[9], 0, 0]) #-
         rightArmAngles["hand"] = self.rightArmAngles["hand"]
 
         headAngles["torso"] = self.headAngles["torso"]
@@ -311,7 +312,7 @@ class Robot(object):
         return left_distances, right_distances, head_distances
 
 if __name__ == "__main__":
-    file_path = "./robot_configuration_files/gen3.yaml"
+    file_path = "./robot_configuration_files/nao.yaml"
     qt = Robot()
     qt.import_robot(file_path)
     # angles = np.array([ 1.52367249, -1.29154365, -0.2268928, -1.54636169, -1.41022609, -0.14137168, 0.03839724, 0.00523599])
@@ -357,9 +358,11 @@ if __name__ == "__main__":
     right = []
     head = []
     for i in range(0, 370, 10):
-        angles = np.array([
-        np.deg2rad(82.5 + i), np.deg2rad(45 + i), np.deg2rad(90), np.deg2rad(0), np.deg2rad(0), np.deg2rad(0), np.deg2rad(0),
-        np.deg2rad(-82.5), np.deg2rad(45), np.deg2rad(90), np.deg2rad(i), np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)])
+        # NEGATIVE ANGLES RIGHT: SHOULDER_ROLL, ELBOWS and wrist
+        angles = np.array([np.deg2rad(80), np.deg2rad(20), np.deg2rad(-80), np.deg2rad(-60), np.deg2rad(0),
+                           np.deg2rad(110), np.deg2rad(-30), np.deg2rad(90), np.deg2rad(88), np.deg2rad(90),
+                           np.deg2rad(0), np.deg2rad(0)])
+
         # angles = np.array([np.deg2rad(-90), np.deg2rad(50), np.deg2rad(-0), np.deg2rad(-88), np.deg2rad(-100),
         #                np.deg2rad(-90), np.deg2rad(-50), np.deg2rad(-0), np.deg2rad(88), np.deg2rad(-100),
         #                np.deg2rad(0), np.deg2rad(0)])
@@ -367,7 +370,7 @@ if __name__ == "__main__":
         # angles = np.array([np.deg2rad(80.18695652), np.deg2rad(-71.84782609), np.deg2rad(-10.6), np.deg2rad(-73.8), np.deg2rad(-68.94782609), np.deg2rad(-7.34347826), np.deg2rad(23.76086957), np.deg2rad(23.34347826)])
         # angles = np.array([np.deg2rad(79.99130435), np.deg2rad(-71.56521739), np.deg2rad(-11.6), np.deg2rad(-73.3), np.deg2rad(-69.16521739), np.deg2rad(-7.49565217), np.deg2rad(23.67391304), np.deg2rad(22.49565217)])
         # angles = np.array([np.deg2rad(80.5), np.deg2rad(-72.3), np.deg2rad(-9.0), np.deg2rad(-74.6), np.deg2rad(-68.6), np.deg2rad(-7.1), np.deg2rad(23.9), np.deg2rad(24.7)])
-        pos_left, pos_right, pos_head = qt.forward_kinematics_kinova(angles)
+        pos_left, pos_right, pos_head = qt.forward_kinematics_nao(angles)
         left.append(pos_left)
         right.append(pos_right)
         head.append(pos_head)
